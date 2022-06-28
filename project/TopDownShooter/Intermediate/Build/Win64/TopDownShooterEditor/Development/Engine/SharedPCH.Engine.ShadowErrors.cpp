baa878104c3b56a133f799694afd9de319990aee
Roman Severin <thirteenth.angel@mail.ru>
2022-06-28 05:55:17 +0300

    clear

--- project/TopDownShooter/Intermediate/Build/Win64/TopDownShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30137
-#include "F:/ProgramFiles/Projects/Repository/UnrealEngine4/unrealengine4/TopDownShooter/Intermediate/Build/Win64/TopDownShooterEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
--- project/TopDownShooter/Source/TopDownShooter/TopDownShooter.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "TopDownShooter.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownShooter, "TopDownShooter" );
-
-DEFINE_LOG_CATEGORY(LogTopDownShooter)
- 
